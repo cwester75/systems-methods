021e9df3e2aa73fb0b8a95fd5d7b60a93a6bed10
cwester75 <100521391+cwester75@users.noreply.github.com>
2026-03-05 23:05:20 -0600

    Add CalendarSpreadSystem class for trading signals

---
+++ kaufman_systems/arbitrage/calendar_spread_system.py
@@ -0,0 +1,14 @@
+class CalendarSpreadSystem:
+
+    def signal(self, near_contract, far_contract):
+
+        spread = far_contract - near_contract
+
+        if spread > 0:
+            return -1
+        else:
+            return 1
+
+    def position_sizing(self, capital):
+
+        return capital * 0.4
